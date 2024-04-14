bb93eb06e4ae4d06f1d0b0595e198d52fa7871b3
suraj keshri <surajkeshri1912@gmail.com>
2024-04-14 12:56:18 +0530

    first version of our hotels

--- basic_api.js
+++
@@ -1,7 +0,0 @@
-const http=require('http');
-const data=require('./data')
-http.createServer((req,resp)=>{
-resp.writeHead(200,{'Content-Type':'application\json'})
-resp.write(JSON.stringify(data))
-resp.end()
-}).listen(9000)
--- data.js
+++
@@ -1,2 +0,0 @@
-const data={name:'surajkeshri',email:'surajkeshri1912@gmail.com'};
-module.exports=data;

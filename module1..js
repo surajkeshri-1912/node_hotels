db93f464d8dbc27ca4a774eba9f8262034352027
surajkeshri-1912 <166983707+surajkeshri-1912@users.noreply.github.com>
2024-04-19 16:07:43 +0530

    Delete module1..js

--- module1..js
+++
@@ -1,6 +0,0 @@
-const fs=require('fs'); // import file system
-fs.writeFileSync("hello2.txt","this file is created using fs module which is not global")
-// we can also use as :: const fs=require('fs').writeFileSync;  fs("hello2.txt","this is new file")
-console.log(__dirname); // it gives directory of current file
-console.log(__filename); //it gives name of current file
-fs.writeFileSync("code.txt","hii this new file")

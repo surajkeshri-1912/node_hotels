0320b698a0664f88bd8735f14923fdec8c5badea
suraj keshri <surajkeshri1912@gmail.com>
2024-04-19 15:11:48 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose=require('mongoose')
+require('dotenv').config();
 //define the mongodb connection url
-const mongoURL='mongodb://localhost:27017/hotels' // replace hotels with your database name
+// const mongoURL='mongodb://localhost:27017/hotels' // replace hotels with your database name , connect to local database
+const mongoURL=process.env.MONGODB_URL;
 mongoose.connect(mongoURL);
 const db=mongoose.connection // mongoose maintain a default connection object representing the mongodb connection
-// definr event listner for database connection
+// define event listner for database connection
 db.on('connected',()=>{
     console.log('connected to mongodb server')
 });
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const app=express();
 const db=require('./db');
+require('dotenv').config();
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json()); // req.body (store)
-
+const PORT=process.env.PORT || 2000;
 app.get('/',(req,res)=>{
     res.send('welcome to our hotel')
 })
@@ -16,7 +17,7 @@ const menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/person',personRoutes)
 app.use('/menu',menuItemRoutes);
 
-app.listen(2000,()=>{
+app.listen(PORT,()=>{
     console.log("listening on port number 2000")
 })
 //
